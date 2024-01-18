8d9cef6f7bc3f51a5b545595d01d5c2dd8651f77
Teddy Lee <teddy.cc.lee@linecorp.com>
2024-01-18 17:15:56 +0800

    test

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests
 
 
 def main(argv):
+    # https://docs.github.com/en/rest/pulls/review-requests?apiVersion=2022-11-28#get-all-requested-reviewers-for-a-pull-request
     token = argv[0]
     # requests.get()
     print(len(argv[0]))
